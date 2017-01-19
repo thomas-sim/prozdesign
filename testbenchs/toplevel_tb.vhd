060e49bffc50e6b2ce74571ac4dc80fc7ac29020
Thomas Simatic <thomas.sim@mailo.com>
2017-01-19 12:46:42 +0100

    fix : wrong test_bench

--- testbenchs/toplevel_tb.vhd
+++ testbenchs/toplevel_tb.vhd
@@ -6,7 +6,7 @@
 -- Author     : Burkart Voss  <bvoss@Troubadix>
 -- Company    : 
 -- Created    : 2015-06-23
--- Last update: 2015-06-23
+-- Last update: 2017-01-19
 -- Platform   : 
 -- Standard   : VHDL'87
 -------------------------------------------------------------------------------
@@ -32,31 +32,44 @@ end toplevel_tb;
 
 architecture behaviour of toplevel_tb is
 
-  component toplevel
+  component toplevel is
     port (
-      reset    : in  STD_LOGIC;
-      clk      : in  STD_LOGIC;
-      w_e_SREG : out std_logic_vector(7 downto 0);
-      Status   : out STD_LOGIC_VECTOR (7 downto 0));
-  end component;
+      clk        : in  std_logic;
+      btnEnter   : in  std_logic;
+      btnR       : in  std_logic;
+      btnU       : in  std_logic;
+      btnD       : in  std_logic;
+      btnL       : in  std_logic;
+      sw         : in  std_logic_vector(15 downto 0);
+      led        : out std_logic_vector(15 downto 0);
+      seg        : out std_logic_vector(7 downto 0);
+      seg_enable : out std_logic_vector(3 downto 0)); 
+  end component toplevel;
 
   -- component ports
   signal reset    : STD_LOGIC;
   signal clk      : STD_LOGIC:='0';
-  signal w_e_SREG : std_logic_vector(7 downto 0);
-  signal Status   : STD_LOGIC_VECTOR (7 downto 0);
 
+  signal btnR : std_logic:='0';
+  signal btnU : std_logic:='0';
+  signal btnD : std_logic:='0';
+  signal btnL : std_logic:='0';
+  signal btnEnter : std_logic:='0';
+
+  signal sw : std_logic_vector(15 downto 0):= (others => '0');
 
 begin  -- behaviour
 
   -- component instantiation
   DUT: toplevel
     port map (
-      reset    => reset,
-      clk      => clk,
-      w_e_SREG => w_e_SREG,
-      Status   => Status);
-
+      clk        => clk,
+      btnEnter   => btnEnter,
+      btnR       => btnR,
+      btnU       => btnU,
+      btnD       => btnD,
+      btnL       => btnL,
+      sw         => sw);
   -- clock generation
   clk <= not clk after 10 ns;
 
@@ -65,9 +78,17 @@ begin  -- behaviour
   begin
     -- insert signal assignments here
     wait for 20ns;
-    reset <= '1';
-    wait for 101ns;
-    reset <= '0';
+    btnR <= '1';
+    btnU <= '1';
+    btnD <= '1';
+    btnL <= '1';
+    btnEnter <= '1';
+    wait for 20ns;
+    btnR <= '0';
+    btnU <= '0';
+    btnD <= '0';
+    btnL <= '0';
+    btnEnter <= '0';
     wait;
 
   end process WaveGen_Proc;
